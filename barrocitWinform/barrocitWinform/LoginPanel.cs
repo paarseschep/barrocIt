ce3eba84ba1032265564781d91fbd2446939e3a4
Nico Steenvoorden <drawolf999@gmail.com>
2014-10-13 14:51:58 +0200

    Made the new login system
    Needs some testing since I'm unable to open the database file.

--- barrocitWinform/barrocitWinform/LoginPanel.cs
+++ barrocitWinform/barrocitWinform/LoginPanel.cs
@@ -13,20 +13,58 @@ namespace barrocitWinform
 {
     public partial class LoginPanel : Form
     {
-        SqlConnection connection;
 
         public LoginPanel()
         {
             InitializeComponent();
+
+            SqlConnector.connection = new SqlConnection (   @"Data Source=(LocalDB)\v11.0;
+                                                            AttachDbFilename=C:\Users\Drace\Documents\GitHub\barrocIt\barrocitWinform\barrocitWinform\Database.mdf;
+                                                            Integrated Security=True;
+                                                            Connect Timeout=30");
         }
 
         private void BtLogin_Click(object sender, EventArgs e)
         {
-            //Login check stuff here
-            connection = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Drace\Documents\GitHub\barrocIt\barrocitWinform\barrocitWinform\Database.mdf;Integrated Security=True;Connect Timeout=30");
+            switch (Connect())
+            {
+                case true:
+                    {
+                        switch (SqlConnector.Login(tbUsername.Text, tbPassword.Text))
+                        {
+                            case 0:
+                                AdminPanel adminPanel = new AdminPanel(this, tbUsername.Text);
+                                adminPanel.Show();
+                                this.Hide();
+                                break;
+                        }
+                        break;
+                    }
+            }
+            
+            
+
+        }
+        public bool Connect()
+        {
+            bool gotConnected = false;
+            try
+            {
+                SqlConnector.connection.Open();
+                gotConnected = true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Failed to connect to server.");
+            }
+            return gotConnected;
+        }
+
+        private void Login()
+        {
+
             if (tbUsername.Text != "" && tbPassword.Text != "")
             {
-                
                 int departmentNumber = 0;
                 string userName = "TestUserName";
                 OpenUserPanel(departmentNumber, userName);
--- barrocitWinform/barrocitWinform/SqlConnector.cs
+++ barrocitWinform/barrocitWinform/SqlConnector.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace barrocitWinform
 {
     static class SqlConnector
     {
-        static public void Login(string userName, string Password)
+        public static SqlConnection connection; 
+
+        static public int Login(string userName, string password)
         {
+            int department;
+            try
+            {
+                SqlDataReader dataReader;
+                SqlCommand loginCommand = new SqlCommand(
+                    "SELECT * FROM Tbl_Users" +
+                    "WHERE Username = " + userName +
+                    " AND Password = " + password,
+                    connection);
+
+                dataReader = loginCommand.ExecuteReader();
+                department = (int)dataReader["department_id"];
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message, "Failed to connect to server.");
+                department = -1;
+            }
+            return department;
         }
     }
 }
