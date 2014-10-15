a68710651cce5fa555ac293251a06def4d41ae51
Nico Steenvoorden <drawolf999@gmail.com>
2014-10-15 14:44:42 +0200

    Added the department panel opening function to the admin panel

--- barrocitWinform/barrocitWinform/AdminPanel.Designer.cs
+++ barrocitWinform/barrocitWinform/AdminPanel.Designer.cs
@@ -28,51 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnFinances = new System.Windows.Forms.Button();
-            this.btnSales = new System.Windows.Forms.Button();
-            this.btnDevelopment = new System.Windows.Forms.Button();
-            this.btnUsers = new System.Windows.Forms.Button();
             this.btnEditUsers = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnDevelopment = new System.Windows.Forms.Button();
+            this.btnFinance = new System.Windows.Forms.Button();
+            this.btnSales = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnFinances
-            // 
-            this.btnFinances.Location = new System.Drawing.Point(385, 176);
-            this.btnFinances.Name = "btnFinances";
-            this.btnFinances.Size = new System.Drawing.Size(182, 41);
-            this.btnFinances.TabIndex = 5;
-            this.btnFinances.Text = "Finances\r\nDepartment";
-            this.btnFinances.UseVisualStyleBackColor = true;
-            // 
-            // btnSales
-            // 
-            this.btnSales.Location = new System.Drawing.Point(197, 176);
-            this.btnSales.Name = "btnSales";
-            this.btnSales.Size = new System.Drawing.Size(182, 41);
-            this.btnSales.TabIndex = 4;
-            this.btnSales.Text = "Sales\r\nDepartment";
-            this.btnSales.UseVisualStyleBackColor = true;
-            // 
-            // btnDevelopment
-            // 
-            this.btnDevelopment.Location = new System.Drawing.Point(11, 176);
-            this.btnDevelopment.Name = "btnDevelopment";
-            this.btnDevelopment.Size = new System.Drawing.Size(182, 41);
-            this.btnDevelopment.TabIndex = 3;
-            this.btnDevelopment.Text = "Development\r\nDepartment";
-            this.btnDevelopment.UseVisualStyleBackColor = true;
-            // 
-            // btnUsers
-            // 
-            this.btnUsers.Location = new System.Drawing.Point(11, 86);
-            this.btnUsers.Name = "btnUsers";
-            this.btnUsers.Size = new System.Drawing.Size(182, 41);
-            this.btnUsers.TabIndex = 2;
-            this.btnUsers.Text = "Edit users";
-            this.btnUsers.UseVisualStyleBackColor = true;
             // 
             // btnEditUsers
             // 
@@ -84,53 +44,56 @@
             this.btnEditUsers.Text = "Edit users";
             this.btnEditUsers.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnDevelopment
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(12, 196);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 47);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Development\r\ndepartment";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnDevelopment.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDevelopment.Location = new System.Drawing.Point(12, 196);
+            this.btnDevelopment.Name = "btnDevelopment";
+            this.btnDevelopment.Size = new System.Drawing.Size(134, 47);
+            this.btnDevelopment.TabIndex = 9;
+            this.btnDevelopment.Text = "Development\r\ndepartment";
+            this.btnDevelopment.UseVisualStyleBackColor = true;
+            this.btnDevelopment.Click += new System.EventHandler(this.btnDevelopment_Click);
             // 
-            // button2
+            // btnFinance
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(222, 196);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 47);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Finance\r\ndepartment";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnFinance.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFinance.Location = new System.Drawing.Point(222, 196);
+            this.btnFinance.Name = "btnFinance";
+            this.btnFinance.Size = new System.Drawing.Size(134, 47);
+            this.btnFinance.TabIndex = 10;
+            this.btnFinance.Text = "Finance\r\ndepartment";
+            this.btnFinance.UseVisualStyleBackColor = true;
+            this.btnFinance.Click += new System.EventHandler(this.btnFinance_Click);
             // 
-            // button3
+            // btnSales
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(438, 196);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(134, 47);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Sales\r\ndepartment";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSales.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSales.Location = new System.Drawing.Point(438, 196);
+            this.btnSales.Name = "btnSales";
+            this.btnSales.Size = new System.Drawing.Size(134, 47);
+            this.btnSales.TabIndex = 11;
+            this.btnSales.Text = "Sales\r\ndepartment";
+            this.btnSales.UseVisualStyleBackColor = true;
+            this.btnSales.Click += new System.EventHandler(this.btnSales_Click);
             // 
             // AdminPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 362);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSales);
+            this.Controls.Add(this.btnFinance);
+            this.Controls.Add(this.btnDevelopment);
             this.Controls.Add(this.btnEditUsers);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "AdminPanel";
             this.Text = "AdminPanel";
             this.Controls.SetChildIndex(this.btnEditUsers, 0);
-            this.Controls.SetChildIndex(this.button1, 0);
-            this.Controls.SetChildIndex(this.button2, 0);
-            this.Controls.SetChildIndex(this.button3, 0);
+            this.Controls.SetChildIndex(this.btnDevelopment, 0);
+            this.Controls.SetChildIndex(this.btnFinance, 0);
+            this.Controls.SetChildIndex(this.btnSales, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,14 +101,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnUsers;
-        private System.Windows.Forms.Button btnDevelopment;
-        private System.Windows.Forms.Button btnSales;
-        private System.Windows.Forms.Button btnFinances;
         private System.Windows.Forms.Button btnEditUsers;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnDevelopment;
+        private System.Windows.Forms.Button btnFinance;
+        private System.Windows.Forms.Button btnSales;
 
     }
 }
--- barrocitWinform/barrocitWinform/DepartmentPanel.cs
+++ barrocitWinform/barrocitWinform/DepartmentPanel.cs
@@ -10,12 +10,15 @@ using System.Windows.Forms;
 
 namespace barrocitWinform
 {
-     public partial class DepartmentPanel : Form
+    public partial class DepartmentPanel : Form
     {
+        public string closeMessage;
+        protected string userName;
         protected Form lastPanel;
         public DepartmentPanel()
         {
             InitializeComponent();
+            closeMessage = "Are you sure you want to log out?";
         }
 
         protected void UpdateGreeting(string userName)
@@ -34,7 +37,7 @@ namespace barrocitWinform
 
         private void DepartmentPanel_FormClosing(object sender, FormClosingEventArgs e)
         {
-            DialogResult dialogResult = MessageBox.Show("Are you sure you want to log out?", "Confirmation", MessageBoxButtons.YesNo);
+            DialogResult dialogResult = MessageBox.Show(closeMessage, "Confirmation", MessageBoxButtons.YesNo);
             if (dialogResult == DialogResult.No)
             {
                 e.Cancel = true;
@@ -44,6 +47,10 @@ namespace barrocitWinform
         {
             lastPanel.Visible = true;
         }
-
+        public void SetCloseMessage(string closeButtonText, string closeMessage)
+        {
+            this.btnLogout.Text = closeButtonText;
+            this.closeMessage = closeMessage;
+        }
     }
 }
