4c65b4d8392942513b573bd253f5934d210ccc4a
Nico Steenvoorden <drawolf999@gmail.com>
2014-11-05 14:17:53 +0100

    Improved regex

--- barrocitWinform/barrocitWinform/DevelopmentMainPanel.cs
+++ barrocitWinform/barrocitWinform/DevelopmentMainPanel.cs
@@ -21,31 +21,16 @@ namespace barrocitWinform
             SetBackButtonType(logoutWhenClose);
         }
 
-        private void btnAddInvoice_Click(object sender, EventArgs e)
+        private void btnViewProject_Click(object sender, EventArgs e)
         {
-            NewProjectPanel viewpanel = new NewProjectPanel(this, userName);
-            viewpanel.Show();
+            ViewPanel viewpanel = new ViewPanel(this, userName, "Tbl_Projects", true);
             this.Hide();
         }
 
-        private void btnViewInvoice_Click(object sender, EventArgs e)
+        private void btnEdit_Click(object sender, EventArgs e)
         {
-            ViewPanel viewpanel = new ViewPanel(this, userName, "Tbl_Customers", true);
-            TextBox myText = new TextBox();
-            Button myBut = new Button();
-            myBut.Size = new Size(100, 20);
-            myBut.Location = new Point(120, 2);
-            myBut.Text = "Go!";
-            myText.Location = new Point(12, 2);
-            viewpanel.Controls.Add(myText);
-            viewpanel.Controls.Add(myBut);
-            viewpanel.Show();
+            ViewPanel viewpanel = new ViewPanel(this, userName, "Tbl_Projects", true);
             this.Hide();
-        }
-
-        private void btEdit_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
